904fc325d02bc7f4f1978123ac6098f351e76f1d
David Lindahl <davidalindahl@gmail.com>
2018-02-27 16:06:23 -0800

    adding config file for contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN,
+      },
+    },
     'gatsby-plugin-offline',
   ],
 };
