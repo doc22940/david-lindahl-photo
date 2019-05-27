482972ad89e9df4a8214129fd82e5edac48067c9
David Lindahl <davidalindahl@gmail.com>
2019-05-27 08:02:36 -0700

    Adding contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { config, useSpring } from 'react-spring'
+import Layout from '../components/layout'
+import { AnimatedBox } from '../elements'
+import SEO from '../components/SEO'
+
+const Contact = () => {
+  const pageAnimation = useSpring({
+    config: config.slow,
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  })
+
+  return (
+    <Layout>
+      <SEO title="Contact | David Lindahl Photography" desc="Reach out to David Lindahl Photography" />
+      <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
+        <h1>Reach out</h1>
+        <p>
+I'm available for local projects as well as potential employment opportunities. Use the form to inquire about rates and availability, or just to say hi.
+        </p>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </AnimatedBox>
+    </Layout>
+  )
+}
+
+export default Contact
